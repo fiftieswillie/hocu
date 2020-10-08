7427375362152fd44780cdb69360c041fb42c7f9
fiftieswillie <68967256+fiftieswillie@users.noreply.github.com>
2020-10-08 13:47:48 +0200

    Update contact.php

--- contact.php
+++ contact.php
@@ -4,7 +4,7 @@
  */
 
 // an email address that will be in the From field of the email.
-$from = 'Demo contact form <info@hocu.nl>';
+$from = 'Demo contact form <info@github.com>';
 
 // an email address that will receive the email with the output of the form
 $sendTo = 'Demo contact form <fiftieswillie@hotmail.com>';
